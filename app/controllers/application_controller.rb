e491599008744de393d92f24e5ee464ebc8cca6b
Bowen Masco <bowen@codingfoo.com>
2013-05-16 15:27:53 -0600

    remove unused helper after refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def item_content(content)
-    CGI.unescapeHTML(CGI.unescape(content.to_html)).html_safe
-  end
-  helper_method :item_content
 end
