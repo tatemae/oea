4294a58d6a433d3e86fbab65be968dee25677f39
Bowen Masco <bowen@codingfoo.com>
2013-05-16 13:42:35 -0600

    add items controller and items index specs

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,19 +1,15 @@
 require 'spec_helper'
 
 describe ItemsController do
-
   describe "GET 'index'" do
     it "returns http success" do
       get 'index'
       response.should be_success
     end
-  end
-
-  describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
+    it "assigns @items" do
+      item1, item2 = Item.create!, Item.create!
+      get :index
+      expect(assigns(:items)).to match_array([item1, item2])
     end
   end
-
 end
--- spec/views/items/index.html.erb_spec.rb
+++ spec/views/items/index.html.erb_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe "items/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "items/index" do
+    it "renders _item partial for each item" do
+      assign(:items, [stub_model(Item), stub_model(Item)])
+      render
+      expect(view).to render_template(:partial => "_item", :count => 2)
+    end
+  end
 end
