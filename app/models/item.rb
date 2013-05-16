6e2a121bb6d99c1df6f7f710ca6a8cd5cada6cb6
Bowen Masco <bowen@codingfoo.com>
2013-05-16 14:38:29 -0600

    move methods into spec

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,28 @@
 class Item < ActiveRecord::Base
-
-
-  def process_xml(xml)
+  def question_text
+    text = ""
+    parsed_xml.css('presentation/material/mattext').each do |question_text|
+      text = item_content(question_text)
+    end
+    text
   end
+
+  def answers
+    parsed_xml.css('response_lid/render_choice/response_label').map do |answer|
+      Answer.new( answer.first[1], item_content(answer.css('mattext')[0]) )
+    end
+  end
+
+  private
+
+  def parsed_xml
+    Nokogiri::XML.parse(self.xml)
+  end
+
+  def item_content(content)
+    CGI.unescapeHTML(CGI.unescape(content.to_html)).html_safe
+  end
+end
+
+class Answer < Struct.new(:id, :text)
 end
