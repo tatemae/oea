c0e7867476877887446ef6d30f2c95acfe6eea94
James Carbine <JamesCarbine@gmail.com>
2013-05-20 13:04:49 -0600

    now after you submit a question, it will re-render with your selected answer

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,8 +10,9 @@ class ItemsController < ApplicationController
   def check_answer
     @item = Item.find(params[:item][:id])
     if @item
+      @selected_answer_id = params["#{@item.id}"]
       @item_result = ItemResult.create!(:identifier => params[:identifier], :item_id => @item.id, :datestamp => Time.now, :referer => request.referer, :ip_address => request.ip)
-      if @item.is_correct? params["#{@item.id}"]
+      if @item.is_correct?(@selected_answer_id)
         flash[:notice] = 'Correct'
       else
         flash[:notice] = 'Incorrect'
