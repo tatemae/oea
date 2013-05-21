00d1da3639e6aa5d6330ee22619d6cb93e695c2d
Kami Wilson <kamijeanwilson@gmail.com>
2013-05-21 15:12:53 -0600

    submit answer using ajax

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,7 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function() {
-  $('.embed_code').slideUp('slow', function(){})
+  $('.embed_code').slideUp('slow', function(){});
   $('.embed_button').on('click', function(e){
     e.preventDefault();
     var $self = $(this).closest('.question');
@@ -11,6 +11,29 @@ $(document).ready(function() {
       });
     } else {
       $self.find('.embed_code').slideUp();
+    }
+  });
+  $('.btn-check-answer').on('click', function(e){
+    e.preventDefault();
+  });
+  $('.radio').on('click', function(e){
+    var $button = $(this).closest('.question').find('.btn-check-answer');
+    if($button.hasClass('disabled')){
+      $button.removeClass('disabled');
+      $button.on('click', function(e){
+        e.preventDefault();
+        $self = $(this).closest('.question');
+        $.ajax({
+          type: "POST",
+          url:  $self.find('form').attr('action') + "?&authenticity_token=" + $self.find('input[name=authenticity_token]').attr('value'),
+          data: $self.find('form').serialize(),
+          dataType: 'json',
+          success: function(data, status){
+          },
+          error: function(jqxhr, status, errorThrown){
+          }
+        });
+      });
     }
   });
 });
