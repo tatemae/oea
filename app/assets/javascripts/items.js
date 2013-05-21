d84ab91f1fdd9b52c9369a3132389d5f7ab3ed78
James Carbine <JamesCarbine@gmail.com>
2013-05-20 23:52:49 -0600

    added datetime rendered to item_result when it is created. added code to show and hide embed code

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,2 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+  $('.embed_code').slideUp('slow', function(){})
+  $('.embed_button').on('click', function(e){
+    e.preventDefault();
+    var $self = $(this).closest('.question');
+    if ($self.find('.embed_code').is(":hidden")) {
+      $self.find('.embed_code').show("slow", function(){
+        $self.find('.embed_text').select();
+      });
+    } else {
+      $self.find('.embed_code').slideUp();
+    }
+  });
+});
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,17 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.all
+    @rendered_time = Time.now
   end
 
   def show
     @item = Item.find(params[:id])
+    @rendered_time = Time.now
   end
 
   def check_answer
     @item = Item.find(params[:item][:id])
+    @rendered_time = Time.now
     if @item
       @selected_answer_id = params["#{@item.id}"]
       if !@user = User.find_by_name(request.session.id)
@@ -16,7 +19,13 @@ class ItemsController < ApplicationController
         @user.name = request.session.id
         @user.save!
       end
-      @user.item_results.create!(:identifier => params[:identifier], :item_id => @item.id, :datestamp => Time.now, :referer => request.referer, :ip_address => request.ip)
+      @user.item_results.create!(
+        :identifier => @item.identifier,
+        :item_id => @item.id,
+        :rendered_datestamp => params[:item][:rendered_time],
+        :datestamp => Time.now,
+        :referer => request.referer,
+        :ip_address => request.ip)
       if @item.is_correct?(@selected_answer_id)
         flash[:persistent_alert] = 'Correct'
       else
