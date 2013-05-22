5475ccd8e6bf06bab6735084a600f5efd88b131e
Bowen Masco <bowen@codingfoo.com>
2013-05-22 17:52:34 -0600

    change api results to scope to item

--- spec/controllers/api/results_controller_spec.rb
+++ spec/controllers/api/results_controller_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
 describe Api::ResultsController do
+  before do
+    @item = Item.new
+    @item.save!
+  end
 
   describe "GET 'index'" do
     it "returns http success" do
-      get 'index', format: :json
+      get 'index', format: :json, item_id: @item.id
       response.should be_success
     end
   end
