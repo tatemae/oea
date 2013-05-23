953f852c008bdd2aeec8813a7bf8bae9d28e222e
Bowen Masco <bowen@codingfoo.com>
2013-05-22 21:54:59 -0600

    create items for a quiz

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -14,4 +14,22 @@ class Api::ItemsController < ApplicationController
     end
     respond_with(item)
   end
+
+  def create_questions
+    xml = Nokogiri::XML.parse(request.body.read)
+    xml.css('item').each do |item_xml|
+      identifier = item_xml.xpath('@ident').to_s
+      if item = Item.find_by_identifier(identifier)
+        item.update_attributes(:xml => item_xml.to_xml)
+        puts "Updated item #{identifier}"
+      else
+        item = Item.new
+        item.identifier = item_xml.xpath('@ident').to_s
+        item.xml = item_xml.to_xml
+        item.save!
+        puts "Created item #{identifier}"
+      end
+    end
+    render nothing: true
+  end
 end
