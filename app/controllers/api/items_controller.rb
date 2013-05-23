d99eb13cacc86685620f8601d48e3d51a3413ebd
Bowen Masco <bowen@codingfoo.com>
2013-05-22 21:54:47 -0600

    dont create item if it already exists

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -5,10 +5,13 @@ class Api::ItemsController < ApplicationController
 
   def create
     item_xml = Nokogiri::XML.parse(request.body.read)
-    item = Item.new
-    item.identifier = item_xml.xpath('//item/@ident').to_s
-    item.xml = item_xml.to_xml
-    item.save!
+    identifier = item_xml.xpath('//item/@ident').to_s
+    unless item = Item.find_by_identifier(identifier)
+      item = Item.new
+      item.identifier = identifier
+      item.xml = item_xml.to_xml
+      item.save!
+    end
     respond_with(item)
   end
 end
