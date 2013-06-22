f67db8070398843bcbe5974f74a17b17131bb5f2
Bowen Masco <bowen@codingfoo.com>
2013-06-21 22:41:29 -0600

    clean up section spec

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -2,15 +2,20 @@ require 'spec_helper'
 
 describe Section do
   before do
-    @xml = open('./spec/fixtures/section.xml')
+    @xml = open('./spec/fixtures/section.xml').read
     @section = Section.new( xml: @xml )
+    @section.save!
   end
 
   it 'should extract the identifier' do
-    @section.identifier.should match /S1002/
+    @section.identifier.should eq 'S1002'
   end
 
   it 'should extract the identifier' do
-    @section.title.should match /Main/
+    @section.title.should eq 'Main'
+  end
+
+  it 'should extract the items' do
+    @section.items.count.should eq 1
   end
 end
