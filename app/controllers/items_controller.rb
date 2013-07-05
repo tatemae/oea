832ae77701cf4362558f18e8f5285720d3d9dc75
James Carbine <JamesCarbine@gmail.com>
2013-07-05 16:24:26 -0600

    removed 'if @item' since it should always be there and it if isn't it will be caught in the rescue

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,55 +21,55 @@ class ItemsController < ApplicationController
   def check_answer
     @item = Item.find(params[:item][:id])
     @rendered_time = Time.now
-    if @item
-      @selected_answer_id = params["#{@item.id}"]
-      if !@user = User.find_by(name: request.session.id)
-        @user = User.create_anonymous
-        @user.name = request.session.id
-        @user.save!
-      end
-      create_result = false
-      if !params[:item][:item_result_id].blank? && @item_result = ItemResult.find(params[:item][:item_result_id])
-        if @item_result.user.id == @user.id
-          @item_result.datestamp = Time.now
-          @item_result.item_variable = [{
-            "response_variable"=>{
-              "id"=>@item.id,
-              "correct_response"=>@item.correct_responses,
-              "base_type"=>@item.base_type,
-              "candidate_response"=>@selected_answer_id
-            }
-          }]
-          @item_result.referer = params[:item][:referer]
-          @item_result.session_status = 'final'
-          @item_result.save!
-        else
-          create_result = true
-        end
+
+    @selected_answer_id = params["#{@item.id}"]
+    if !@user = User.find_by(name: request.session.id)
+      @user = User.create_anonymous
+      @user.name = request.session.id
+      @user.save!
+    end
+    create_result = false
+    if !params[:item][:item_result_id].blank? && @item_result = ItemResult.find(params[:item][:item_result_id])
+      if @item_result.user.id == @user.id
+        @item_result.datestamp = Time.now
+        @item_result.item_variable = [{
+          "response_variable"=>{
+            "id"=>@item.id,
+            "correct_response"=>@item.correct_responses,
+            "base_type"=>@item.base_type,
+            "candidate_response"=>@selected_answer_id
+          }
+        }]
+        @item_result.referer = params[:item][:referer]
+        @item_result.session_status = 'final'
+        @item_result.save!
       else
         create_result = true
       end
-      if create_result
-        @item_result = @user.item_results.create!(
-          :identifier => @item.identifier,
-          :item_id => @item.id,
-          :rendered_datestamp => params[:item][:rendered_time],
-          :datestamp => Time.now,
-          :referer => params[:item][:referer],
-          :ip_address => request.ip,
-          :session_status => 'final',
-          :item_variable => [{
-            "response_variable"=>{
-              "id"=>@item.id,
-              "correct_response"=>@item.correct_responses,
-              "base_type"=>@item.base_type,
-              "candidate_response"=>@selected_answer_id
-            }
-          }])
-      end
-      feedback = @item.feedback(@selected_answer_id)
-      @result = @item.is_correct?(@selected_answer_id)
+    else
+      create_result = true
     end
+    if create_result
+      @item_result = @user.item_results.create!(
+        :identifier => @item.identifier,
+        :item_id => @item.id,
+        :rendered_datestamp => params[:item][:rendered_time],
+        :datestamp => Time.now,
+        :referer => params[:item][:referer],
+        :ip_address => request.ip,
+        :session_status => 'final',
+        :item_variable => [{
+          "response_variable"=>{
+            "id"=>@item.id,
+            "correct_response"=>@item.correct_responses,
+            "base_type"=>@item.base_type,
+            "candidate_response"=>@selected_answer_id
+          }
+        }])
+    end
+    feedback = @item.feedback(@selected_answer_id)
+    @result = @item.is_correct?(@selected_answer_id)
+
     words = feedback
     if @result
       words = ['correct'] if feedback.empty?
@@ -90,6 +90,7 @@ class ItemsController < ApplicationController
     respond_to do |format|
       format.json { render :json => result }
     end
+
   rescue => e
     respond_to do |format|
       format.json { render :json => e.to_s }
