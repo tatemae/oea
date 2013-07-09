b0d92c03bde2f2b61e4533b9bcccbbcb632077b5
Bowen Masco <bowen@codingfoo.com>
2013-07-09 16:17:37 -0600

    spec for domain scoping

--- spec/controllers/api/item_results_controller_spec.rb
+++ spec/controllers/api/item_results_controller_spec.rb
@@ -3,12 +3,20 @@ require 'spec_helper'
 describe Api::ItemResultsController do
   before do
     @item = FactoryGirl.create(:item)
+    @result1 = FactoryGirl.create(:item_result, item: @item)
+    @result2 = FactoryGirl.create(:item_result, item: @item, referer: 'www.codingfoo.com')
   end
 
   describe "GET 'index'" do
     it "returns http success" do
       get :index, format: :json, id: @item.id
       response.should be_success
+    end
+
+    it "scopes results" do
+      result = { item_results: [ { item_results: @result2 } ] }
+      get :index, format: :json, id: @item.id, scope: 'domain', url: 'www.codingfoo.com'
+      expect(response.body).to eq result.to_json
     end
   end
 
