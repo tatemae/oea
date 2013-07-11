f2eaa2f4f49b89c950682877871e4c2366ffe5f6
Justin Ball <justinball@gmail.com>
2013-07-11 17:58:29 -0600

    Fixed scope

--- app/models/assessment_xml.rb
+++ app/models/assessment_xml.rb
@@ -1,6 +1,6 @@
 class AssessmentXml < ActiveRecord::Base
   belongs_to :assessment
 
-  scope :by_newest, -> { order(created_at: 'desc') }
+  scope :by_newest, -> { order(created_at: :desc) }
 
 end
