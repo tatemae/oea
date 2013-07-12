9d56ad02780a1e747c52c2ec87957dac818a3aa5
Bowen Masco <bowen@codingfoo.com>
2013-07-12 08:46:48 -0600

    add saml settings

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -2,28 +2,36 @@ class SamlController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:consume]
 
   def index
-    settings = ENV["saml"]
+    settings = {
+      assertion_consumer_service_url: ENV["assertion_consumer_service_url"]
+      issuer: ENV["issuer"]
+      idp_sso_target_url: ENV["idp_sso_target_url"]
+      idp_cert_fingerprint: ENV["idp_cert_fingerprint"]
+      name_identifier_format: ENV["name_identifier_format"]
+    }
+
     request = Onelogin::Saml::Authrequest.new
     redirect_to(request.create(settings))
   end
 
   def consume
     response = Onelogin::Saml::Response.new(params[:SAMLResponse])
-    response.settings = ENV["saml"]
+    response.settings = {
+      assertion_consumer_service_url: ENV["assertion_consumer_service_url"]
+      issuer: ENV["issuer"]
+      idp_sso_target_url: ENV["idp_sso_target_url"]
+      idp_cert_fingerprint: ENV["idp_cert_fingerprint"]
+      name_identifier_format: ENV["name_identifier_format"]
+    }
 
     logger.info "NAMEID: #{response.name_id}"
 
     if response.is_valid?
-      session[:userid] = response.name_id
-      redirect_to :action => :complete
+      user = User.find_by_email(response.name_id)
+      sign_in(:user, user)
+      redirect_to user_path(user)
     else
-      redirect_to :action => :fail
+      redirect_to new_user_session
     end
-  end
-  
-  def complete
-  end
-  
-  def fail
   end
 end
