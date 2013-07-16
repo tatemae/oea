98f4e10dd4129545c257eb31b488e16a08b5b02f
Justin Ball <justinball@gmail.com>
2013-07-16 12:51:13 -0600

    Formatting

--- app/assets/javascripts/assessment/config/routes.js
+++ app/assets/javascripts/assessment/config/routes.js
@@ -2,5 +2,7 @@ var App = require('./app');
 
 App.Router.map(function(){
   this.resource('assessments');
-  this.resource('assessment', { path: '/assessment/:assessment_id' });
+  this.resource('assessment', { path: '/assessment/:assessment_id' }, function(){
+    this.resource('questions');
+  });
 });
