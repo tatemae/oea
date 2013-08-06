28bdb32d01bbb55435eda92de0e32246b19810bd
Bowen Masco <bowen@codingfoo.com>
2013-08-06 16:04:27 -0600

    initial assessment results

--- app/controllers/assessment_results_controller.rb
+++ app/controllers/assessment_results_controller.rb
@@ -1,5 +1,19 @@
 class AssessmentResultsController < ApplicationController
   def index
-    @assessment = Assessment.find(params[:id])
+    @assessment = Assessment.includes(items: :item_results).find(params[:id])
+
+    @results = @assessment.items.collect(&:item_results).flatten
+    @displayed = @results.select{|i| i.session_status == 'initial' }.count #uniq for user?
+    @unique_users_count = @results.uniq(&:user_id).count
+    @submissions_count = @results.select{|i| i.session_status == 'final' }.count
+    results_summaries = @assessment.items.collect(&:results_summary)
+    percent_correct_array = results_summaries.collect{ |h| h[:percent_correct] }
+    @percent_correct = percent_correct_array.inject(:+).to_f / percent_correct_array.size * 100 #reject zeros?
+
+    avg_time_to_complete_arr = @results.group_by(&:user_id).collect do |k,v|
+      sorted = v.sort_by(&:created_at)
+      diff = sorted.last.created_at.to_i - sorted.first.created_at.to_i
+    end
+    @avg_time_to_complete = avg_time_to_complete_arr.inject(:+).to_f / avg_time_to_complete_arr.size * 100
   end
 end
--- spec/controllers/assessment_results_controller_spec.rb
+++ spec/controllers/assessment_results_controller_spec.rb
@@ -3,10 +3,21 @@ require 'spec_helper'
 describe AssessmentResultsController do
 
   describe "GET 'index'" do
+    let(:assessment){ make_assessment }
+
     it "returns http success" do
-      assessment = Assessment.create! title: "foo title"
       get 'index', :id => assessment.id
       response.should be_success
+    end
+
+    it 'should load the items and item results' do
+      get 'index', :id => assessment.id
+      expect(assigns[:assessment].items.first.item_results.count).to eq 0
+    end
+
+    it 'should load the items and item results' do
+      get 'index', :id => assessment.id
+      expect(assigns[:item_results].items.first.item_results.count).to eq 0
     end
   end
 
