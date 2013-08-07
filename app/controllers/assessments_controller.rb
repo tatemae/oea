bf0cf7e2b2017c10d06cd3c434361c873fbc4e12
James Carbine <JamesCarbine@gmail.com>
2013-08-07 11:32:30 -0600

    increased iframe height

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -33,9 +33,9 @@ class AssessmentsController < ApplicationController
     @display_embed_code = !params[:embed]
 
     if @assessment.src_url
-      @embed_code = CGI.unescapeHTML("<iframe src='//#{request.host_with_port}#{assessments_path}?embed=true&aid=#{current_user.id}&src_url=#{@assessment.src_url}' frameborder='0' width='600' height='400' ></iframe>")
+      @embed_code = CGI.unescapeHTML("<iframe src='//#{request.host_with_port}#{assessments_path}?embed=true&aid=#{current_user.id}&src_url=#{@assessment.src_url}' frameborder='0' width='600' height='500' ></iframe>")
     else
-      @embed_code = CGI.unescapeHTML("<iframe src='//#{request.host_with_port}#{assessment_path(@assessment)}' frameborder='0' width='600' height='400' ></iframe>")
+      @embed_code = CGI.unescapeHTML("<iframe src='//#{request.host_with_port}#{assessment_path(@assessment)}' frameborder='0' width='600' height='500' ></iframe>")
     end
 
     create_item_result(@item)
