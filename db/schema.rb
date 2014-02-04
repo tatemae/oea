0c7ec24c6baba7b69b00e585ad85bb81ed775a16
Justin Ball <justinball@gmail.com>
2014-02-04 15:34:21 -0700

    Updated Schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140204040817) do
+ActiveRecord::Schema.define(version: 20140204212354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(version: 20140204040817) do
     t.datetime "updated_at"
     t.string   "name"
     t.string   "authentication_token"
+    t.string   "external_id"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
