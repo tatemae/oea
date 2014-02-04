dd54b897e3984332e9f7273d3b9c1b9cae97f924
Justin Ball <justinball@gmail.com>
2014-02-03 21:18:30 -0700

    Moved embed show/hide into application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,19 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require items
 
-//= require_tree ../../../vendor/assets/javascripts
+
+$(document).ready(function() {
+  $('.embed_button').on('click', function(e){
+    e.preventDefault();
+    var container = $(e.target).closest('.assessment_meta');
+    if(container.find('.embed_code').is(":hidden")) {
+      container.find('.embed_code').slideDown(function(){
+        container.find('.embed_text').select();
+      });
+    } else {
+      container.find('.embed_code').slideUp();
+    }
+  });
+
+});
