b48437c528ed9b03b59ea3c125a111a7327882a6
Justin Ball <justinball@gmail.com>
2014-02-10 11:33:41 -0700

    Fixed url. Fixed spec

--- spec/controllers/assessment_results_controller_spec.rb
+++ spec/controllers/assessment_results_controller_spec.rb
@@ -2,21 +2,21 @@ require 'spec_helper'
 
 describe AssessmentResultsController do
 
-  describe "GET 'index'" do
+  describe "GET show" do
     let(:assessment){ make_assessment }
 
     it "returns http success" do
-      get 'index', :id => assessment.id
+      get :show, :id => assessment.id
       response.should be_success
     end
 
     it 'should load the items and item results' do
-      get 'index', :id => assessment.id
+      get :show, :id => assessment.id
       expect(assigns[:assessment].items.first.item_results.count).to eq 0
     end
 
     it 'should load the items and item results' do
-      get 'index', :id => assessment.id
+      get :show, :id => assessment.id
       expect(assigns[:item_results].items.first.item_results.count).to eq 0
     end
   end
