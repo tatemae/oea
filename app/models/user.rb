be1de28182ea76dd55c55aaa9e4aa9ab273d2eda
James Carbine <jamescarbine@gmail.com>
2014-02-10 15:19:22 -0700

    generate auth token for users before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   has_many :item_results
   has_many :assessments
 
-  #before_save :ensure_authentication_token
+  before_save :ensure_authentication_token
 
   def display_name
     return self.name if self.name.present?
@@ -26,4 +26,18 @@ class User < ActiveRecord::Base
     user
   end
 
+  def ensure_authentication_token
+    if authentication_token.blank?
+      self.authentication_token = generate_authentication_token
+    end
+  end
+
+  private
+
+  def generate_authentication_token
+    loop do
+      token = Devise.friendly_token
+      break token unless User.where(authentication_token: token).first
+    end
+  end
 end
