7d32ebf4030747a1f588fb896cd46669bb62b0d6
Bowen Masco <bowen@codingfoo.com>
2014-04-10 09:50:28 -0600

    move files

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -4,6 +4,13 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('sections',                                            function(){
+    this.resource('section',  { path: '/:section_id' },                function(){
+      this.resource('items',                                           function(){
+        this.resource('item', { path: '/:item_id' });
+      });
+    });
+  });
 });
 
 export default Router;
