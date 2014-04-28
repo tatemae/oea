d76da1f9914355c0785d0628527d1c2bb7c40040
Justin Ball <justinball@gmail.com>
2014-04-28 16:25:50 -0600

    Use two significant digits on all percentages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :signif
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
