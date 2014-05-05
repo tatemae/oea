89415f1a5d48eb76b6451ddee59a7bfec61c9636
Bowen Masco <bowen@codingfoo.com>
2014-05-05 13:36:56 -0600

    cli-timing test

--- frontend/app/routes/item.js
+++ frontend/app/routes/item.js
@@ -1,6 +1,22 @@
 import ItemResult from "../models/item_result";
 
 export default Ember.Route.extend({
+  beforeModel: function(transition){
+    if(typeof start !== "undefined")
+    {
+      this.endTimeOnQuestion();
+      var item = this.get('currentModel');
+      console.log("time spent" + end - start);
+      Oea.ItemResult.create({
+        assessment: this.modelFor('application'),
+        resultsEndPoint: OEA_SETTINGS.resultsEndPoint,
+        user_id: OEA_SETTINGS.userId,
+        item_id: item.id,
+        identifier: item.id,
+        timeSpent: end - start
+      }).save();
+    }
+  },
 
   model: function(params){
 
@@ -14,6 +30,19 @@ export default Ember.Route.extend({
     }).save();
 
     return this.modelFor('items').findBy('id', params.item_id);
+  },
+
+  afterModel: function(model, transition){
+    this.startTimeOnQuestion();
+  },
+
+  startTimeOnQuestion: function() {
+    this.start = new Date().getTime();
+  },
+
+  endTimeOnQuestion: function() {
+    this.end = new Date().getTime();
   }
+
 
 });
