68dfdd6ffad951c81eb6fc226568d49ee7d827d2
Bowen Masco <bowen@codingfoo.com>
2014-05-17 21:49:20 -0600

    move log settings

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -4,13 +4,6 @@ import loadInitializers from 'ember/load-initializers';
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
-  LOG_STACKTRACE_ON_DEPRECATION : true,
-  LOG_BINDINGS                  : true,
-  LOG_TRANSITIONS               : true,
-  LOG_TRANSITIONS_INTERNAL      : true,
-  LOG_VIEW_LOOKUPS              : true,
-  LOG_ACTIVE_GENERATION         : true,
-
   modulePrefix: 'oea', // TODO: loaded via config
   Resolver: Resolver
 });
