dd0ce6b6d79611fe1487eb96c0d328f05eb55121
KamiJean <kamijeanwilson@gmail.com>
2014-05-26 10:14:11 -0600

    Wrote specs for the new mailer

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,15 @@
 require "spec_helper"
 
 describe UserMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+	include EmailSpec::Helpers
+  include EmailSpec::Matchers
+  before do
+    @user = FactoryGirl.create(:user)
+  end
+  
+  subject { UserMailer.contact_email(@user.email, @user.name).deliver }
+
+  it { should deliver_from "admin@openassessment.com" }
+  it { should deliver_to @user.email }
+  it { should have_subject Regexp.new("Thank you for contacting Open Assessments") }
 end
