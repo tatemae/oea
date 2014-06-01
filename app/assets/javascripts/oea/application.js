ddea9de20c02bd27673d6429e88a67834047fc82
Bowen Masco <bowen@codingfoo.com>
2014-05-31 18:31:59 -0600

    upgrade rails to work with new .27 app

--- app/assets/javascripts/oea/application.js
+++ app/assets/javascripts/oea/application.js
@@ -1,3 +1,3 @@
 //= require ./app
-
-window.Oea = require('oea/main')['default']('oea');
+window.ENV = {"baseURL":"/","locationType":"none","FEATURES":{},"APP":{"LOG_RESOLVER":true,"LOG_ACTIVE_GENERATION":true,"LOG_MODULE_RESOLVER":true,"LOG_TRANSITIONS":true,"LOG_VIEW_LOOKUPS":true,"LOG_STACKTRACE_ON_DEPRECATION":true,"LOG_BINDINGS":true},"LOG_MODULE_RESOLVER":true};
+window.Oea = require('oea/app')['default'].create(ENV.APP);
