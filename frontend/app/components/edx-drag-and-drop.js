114e8748da2f97a339b3dd506c52010737382f08
Justin Ball <justinball@gmail.com>
2014-08-25 16:23:29 -0600

    Added drop targets. Refactored location of drag logic.

--- frontend/app/components/edx-drag-and-drop.js
+++ frontend/app/components/edx-drag-and-drop.js
@@ -1,6 +1,49 @@
 import Ember from 'ember';
+import EdX from "../utils/edx";
 
 export default Ember.Component.extend({
+
+  question: function(){
+    return EdX.buildProblemMaterial(this.get('content.xml'));
+  }.property('content.xml'),
+
+  images: function(){
+    var imgs = Ember.A();
+    var dndRoot = this.get('content.xml').find('drag_and_drop_input');
+    if(dndRoot.length > 0){
+      var image = {
+        src: dndRoot.attr('img'),
+        title: 'Drag and Drop main image'
+      };
+      imgs.pushObject(image);
+    }
+    return imgs;
+  }.property('content.xml'),
+
+  isDroppableImage: function(){
+    return this.get('targets').length <= 0;
+  }.property('targets'),
+
+  draggables: function(){
+    return Ember.$.map(this.get('content.xml').find('draggable'), function(draggable){
+      draggable = Ember.$(draggable);
+      return {
+        'id': draggable.attr('id'),
+        'label': draggable.attr('label')
+      };
+    });
+  }.property('content.xml'),
+
+  targets: function(){
+    return Ember.$.map(this.get('content.xml').find('target'), function(target){
+      target = Ember.$(target);
+      return {
+        'id': target.attr('id'),
+        'label': target.attr('label'),
+        'style': 'top:' + target.attr('y') + 'px;left:' + target.attr('x') + 'px;width:' + target.attr('w') + 'px;height:' + target.attr('h') + 'px;'
+      };
+    });
+  }.property('content.xml'),
 
   didInsertElement: function(){
 
@@ -10,22 +53,24 @@ export default Ember.Component.extend({
       // only accept elements matching this CSS selector
       .accept('.draggable')
       // listen for drop related events
-      .on('dragenter', function (event) {
-          var draggableElement = event.relatedTarget,
-              dropzoneElement = event.target;
+      .on('dragenter', function(event){
+        var draggableElement = event.relatedTarget;
+        var dropzoneElement = event.target;
 
-          // feedback the possibility of a drop
-          dropzoneElement.classList.add('drop-target');
-          draggableElement.classList.add('can-drop');
+        // feedback the possibility of a drop
+        dropzoneElement.classList.add('drop-target');
       })
-      .on('dragleave', function (event) {
-          // remove the drop feedback style
-          event.target.classList.remove('drop-target');
-          event.relatedTarget.classList.remove('can-drop');
+      .on('dragleave', function(event){
+        // remove the drop feedback style
+        event.target.classList.remove('drop-target');
       })
-      .on('drop', function (event) {
-          //event.relatedTarget.textContent = 'Dropped';
+      .on('drop', function(event){
+        // remove the drop feedback style
+        event.target.classList.remove('drop-target');
+        //event.relatedTarget.textContent = 'Dropped';
       });
+
+    //var dropzone = Ember.$('.panel-body');
 
     interact('.draggable')
       .draggable({
@@ -36,14 +81,13 @@ export default Ember.Component.extend({
           target.x = (target.x|0) + event.dx;
           target.y = (target.y|0) + event.dy;
 
-          target.style.webkitTransform = target.style.transform =
-              'translate(' + target.x + 'px, ' + target.y + 'px)';
+          target.style.webkitTransform = target.style.transform = 'translate(' + target.x + 'px, ' + target.y + 'px)';
         }
       })
-      .inertia(true)
-      .restrict({
-        drag: this.$()[0]
-      });
+      .inertia(true);
+      // .restrict({
+      //   drag: dropzone[0]
+      // });
   }
 
 });
--- frontend/app/models/edx_answer.js
+++ frontend/app/models/edx_answer.js
@@ -4,33 +4,6 @@ import EdX from "../utils/edx";
 
 var EdxAnswer = Base.extend({
 
-  question: function(){
-    return EdX.buildProblemMaterial(this.get('xml'));
-  }.property('xml'),
-
-  images: function(){
-    var imgs = Ember.A();
-    var dndRoot = this.get('xml').find('drag_and_drop_input');
-    if(dndRoot.length > 0){
-      var image = {
-        src: dndRoot.attr('img'),
-        title: 'Drag and Drop main image'
-      };
-      imgs.pushObject(image);
-    }
-    return imgs;
-  }.property('xml'),
-
-  draggables: function(){
-    return Ember.$.map(this.get('xml').find('draggable'), function(draggable){
-      draggable = Ember.$(draggable);
-      return {
-        'id': draggable.attr('id'),
-        'label': draggable.attr('label')
-      };
-    });
-  }.property('xml'),
-
   correctAnswer: function(){
     var answer = this.get('xml').find('answer').html();
     answer = answer.replace('correct_answer =', '');
