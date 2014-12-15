8e774f9bc2e46d52551fde17a7dc7ef06f356d1a
Justin Ball <justinball@gmail.com>
2014-12-15 15:12:17 -0700

    Handle encoded urls.

--- app/controllers/assessment_loaders_controller.rb
+++ app/controllers/assessment_loaders_controller.rb
@@ -3,10 +3,20 @@ class AssessmentLoadersController < ApplicationController
   def create
     vals = {}
     [:eid, :keywords, :style, :confidence_levels, :src_url].each do |k|
-      vals[k] = ERB::Util.url_encode(params[k]) unless params[k].blank?
+      vals[k] = ERB::Util.url_encode(ensure_decoded(params[k])) unless params[k].blank?
     end
     vals[:load_ui] = true
     redirect_to assessment_path('load', vals)
   end
+
+  private
+
+    def ensure_decoded(encoded)
+      decoded = encoded
+      begin
+        decoded = URI.decode(decoded) 
+      end while(decoded != URI.decode(decoded))
+      decoded
+    end
 
 end
