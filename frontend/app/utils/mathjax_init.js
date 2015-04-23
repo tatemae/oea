cc405af9b5d32875bf6e8d266800beea2937d20c
Justin Ball <justinball@gmail.com>
2015-04-22 22:41:30 -0600

    Added offline examples

--- frontend/app/utils/mathjax_init.js
+++ frontend/app/utils/mathjax_init.js
@@ -22,21 +22,6 @@ var MathJaxInit = {
     /* see http://www.mathjax.org/docs/1.1/options/tex2jax.html */
     config = 'MathJax.Hub.Config({tex2jax:{inlineMath:[[\'$\',\'$\'], ["\\(","\\)"]],displayMath:[[\'\\\\[\',\'\\\\]\'], ["\\[","\\]"]],processEscapes:true}});MathJax.Hub.Startup.onload();';
 
-
-
-// MathJax.Hub.Config({
-//     extensions: ["tex2jax.js"],
-//     jax: ["input/TeX", "output/HTML-CSS"],
-//     tex2jax: {
-//       inlineMath: [ ['$','$'], ["\\(","\\)"] ],
-//       displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
-//       processEscapes: true
-//     },
-//     "HTML-CSS": { availableFonts: ["TeX"] }
-//   });
-
-
-
     if (window.opera) script.innerHTML = config; else script.text = config;
 
     doc.getElementsByTagName('head')[0].appendChild(script);
